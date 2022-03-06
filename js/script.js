3cb98f21c1226202f8c7b71fba370821589efee0
Marwin_Rop <68537635+Marwin5411-Winner@users.noreply.github.com>
2022-03-06 19:53:18 +0700

    Fix window loacation Protocol om script.js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ convertBtn.addEventListener('click', () => {
 });
 resolveBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
-    document.getElementById('link').innerHTML = `${window.location.protocol}://${window.location.hostname}/video?URL=${URLinput.value}`;
+    document.getElementById('link').innerHTML = `${window.location.protocol}//${window.location.hostname}/video?URL=${URLinput.value}`;
 });
 function sendURL(URL) {
-    window.location.href = `${window.location.protocol}://${window.location.hostname}/video?URL=${URL}`;
+    window.location.href = `${window.location.protocol}//${window.location.hostname}/video?URL=${URL}`;
 }
