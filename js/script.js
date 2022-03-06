90bab6001b51217b6fa96b15659fa76f57825a2e
Marwin_Rop <68537635+Marwin5411-Winner@users.noreply.github.com>
2022-03-06 22:24:09 +0700

    Fix JS Bug

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
 var resolveBtn = document.querySelector('#resolve');
-convertBtn.addEventListener('click', () => {
-    console.log(`URL: ${URLinput.value}`);
-    sendURL(URLinput.value);
-});
 resolveBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
     document.getElementById('link').innerHTML = `${window.location.protocol}//${window.location.hostname}/video?URL=${URLinput.value}`;
