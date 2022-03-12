022e45e6933034b395b11a1c0cffbbe70eff7a19
Marwin_Rop <68537635+Marwin5411-Winner@users.noreply.github.com>
2022-03-13 00:35:24 +0700

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,5 @@ var URLinput = document.querySelector('#videoURL');
 var resolveBtn = document.querySelector('#submit');
 resolveBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
-    document.getElementById('link').innerHTML = `${window.location.protocol}//${window.location.hostname}/video?url=${URLinput.value}`;
+    document.getElementById('link').innerHTML = `${window.location.protocol}//${window.location.hostname}/download?url=${URLinput.value}`;
 });
